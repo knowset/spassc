9ed1d0c4a7d5a6d24447439a3db7e1d6499f0d1f
knowset <knowset@proton.me>
2023-04-09 19:51:14 +0300

    2

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH + "/",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH
 }
 
